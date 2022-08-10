250a2040e0b1f4605085d4f82286a92a6569d7a2
3picII <szymon.golej@gmail.com>
2022-08-10 13:20:13 +0200

    Poprawiony model Entity

--- src/main/java/pl/coderslab/game_objects/Entity.java
+++ src/main/java/pl/coderslab/game_objects/Entity.java
@@ -1,30 +1,23 @@
 package pl.coderslab.game_objects;
 
+import lombok.Data;
+
+import javax.validation.constraints.Size;
+
+@Data
 public class Entity {
     //Entity ma być wyjściowym modelem do wszystkiego, posiada nazwę oraz opis, później dodam do niej pewnie
     //jeszcze jakieś metody
 
+    @Size(min = 2)
     private String name;
+
+    @Size(max = 200)
     private String description;
+
 
     public Entity(String name, String description) {
         this.name = name;
-        this.description = description;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
         this.description = description;
     }
 }
