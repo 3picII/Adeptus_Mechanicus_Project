26975090257eab6c81d2fcbf47398744c56c5359
3picII <szymon.golej@gmail.com>
2022-08-10 12:47:09 +0200

    Poprawiony model przedmiotu

--- src/main/java/pl/coderslab/game_objects/Item.java
+++ src/main/java/pl/coderslab/game_objects/Item.java
@@ -1,10 +1,33 @@
 package pl.coderslab.game_objects;
 
-public class Item extends Entity{
+import org.hibernate.validator.constraints.Range;
+
+import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = Item.TABLE_NAME)
+public class Item{
+
+    final static String TABLE_NAME = "items";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Size(min = 2)
+    private String name;
+
+    @Size(max = 200)
+    private String description;
+
+    @Range(min = 1, max = 2)
+    private int type; //Typ przedmiotu: 1 - przedmiot dodający siłę ataku, 2 - przedmiot zwiększający hp
+
+    @Min(10)
     private int power;
 
-    public Item(String name, String description, int power) {
-        super(name, description);
-        this.power = power;
-    }
+
 }
